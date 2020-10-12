1f4736e32b0882c53710d079ea066ef3c34c3df9
ItsThompson <33135895+ItsThompson@users.noreply.github.com>
2020-10-12 14:45:24 +0800

    🌌 Fixed Spacing & broken get requests.

--- thought/server.js
+++ thought/server.js
@@ -1,39 +1,39 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 var corsOptions = {
-    origin: `http://localhost:8080`
-}
+    origin: `http://localhost:8080`,
+};
 
 app.use(cors(corsOptions));
 
 // Parsers
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-require('./src/app/routes/thought.routes')(app);
+require("./src/app/routes/thought.routes")(app);
 const port = process.env.PORT || 3000;
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`Running on http://localhost:${port}`);
 });
 
-app.get('*', (req, res) => {
-    res.json({message: "Welcome to thought application."})
+app.get("*", (req, res) => {
+    res.json({ message: "Welcome to thought application." });
 });
 
 // Connect to db
 const db = require("./src/app/models");
 db.mongoose
-  .connect(db.url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => {
-    console.log("Connected to the database!");
-  })
-  .catch(err => {
-    console.log("Cannot connect to the database!", err);
-    process.exit();
-  });
+    .connect(db.url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("Connected to the database!");
+    })
+    .catch((err) => {
+        console.log("Cannot connect to the database!", err);
+        process.exit();
+    });
