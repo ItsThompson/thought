105764c85933939851c9f7df6ffd34949adbbf5c
ItsThompson <33135895+ItsThompson@users.noreply.github.com>
2020-10-16 13:13:33 +0800

    ❌ Removed thought-detail component

--- thought/src/app/app-routing.module.ts
+++ thought/src/app/app-routing.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ThoughtsListComponent } from './components/thoughts-list/thoughts-list.component';
-import { ThoughtDetailsComponent } from './components/thought-details/thought-details.component';
 import { AddThoughtComponent } from './components/add-thought/add-thought.component';
 
 const routes: Routes = [
   {path: 'thoughts', component: ThoughtsListComponent},
-  {path: 'thoughts/:id', component: ThoughtDetailsComponent},
   {path: 'add', component: AddThoughtComponent},
   {path: '', redirectTo: 'thoughts', pathMatch: 'full'},
 ];
--- thought/src/app/app.module.ts
+++ thought/src/app/app.module.ts
@@ -6,14 +6,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddThoughtComponent } from './components/add-thought/add-thought.component';
-import { ThoughtDetailsComponent } from './components/thought-details/thought-details.component';
 import { ThoughtsListComponent } from './components/thoughts-list/thoughts-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddThoughtComponent,
-    ThoughtDetailsComponent,
     ThoughtsListComponent
   ],
   imports: [
